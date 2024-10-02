c4ec56bec5dc86f57919b515d73968711dcf3e8c
swarupsahu08 <swarup8sahu@gmail.com>
2024-10-02 20:13:02 +0530

    Added the helloworld program

---
+++ scripts/hello_world_swarupahu08.java
@@ -0,0 +1,10 @@
+// LANGUAGE: Java
+// AUTHOR: Swarup Kumar Sahu
+// GITHUB: https://github.com/swarupsahu08
+
+public class hello_world_swarupahu08 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
